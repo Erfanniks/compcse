ed961f41fd7a704698338cd1f9923ea3dd62e4e6
Erfanniks <34509377+Erfanniks@users.noreply.github.com>
2024-03-26 15:56:38 +0200

    Delete assignment/backend directory

--- assignment/backend/src/tests/mocks/server.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-import { http } from "msw";
-import { setupServer } from "msw/node";
-import { handlers } from "./handlers";
-
-// Setup requests interception using the given handlers.
-const server = setupServer(...handlers);
-
-export { http, server };
--- assignment/backend/src/tests/utils/routerMock.js
+++
@@ -1,9 +0,0 @@
-const mockedUsedNavigate = jest.fn();
-
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useNavigate: () => mockedUsedNavigate
-}));
-
-export * from 'react-router-dom';
-export { mockedUsedNavigate };
